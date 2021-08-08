a8381cec7aada751b698c09dde5d6a87b311ea3e
SamuelParrales <68031754+SamuelParrales@users.noreply.github.com>
2021-08-08 11:41:12 -0500

    Add files via upload

---
+++ arduino/src/main/java/sepp/arduino/ArduinoApplication.java
@@ -0,0 +1,13 @@
+package sepp.arduino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArduinoApplication.class, args);
+	}
+
+}
