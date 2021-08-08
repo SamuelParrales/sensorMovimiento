7b2eaf12f4bb57758da3d12bf6db2903c7d20a4e
SamuelParrales <68031754+SamuelParrales@users.noreply.github.com>
2021-08-08 13:46:40 -0500

    Web socket configuracion

---
+++ arduino/src/main/java/sepp/arduino/SocketTextHandler.java
@@ -0,0 +1,34 @@
+package sepp.arduino;
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SocketTextHandler extends TextWebSocketHandler {
+
+	
+	//
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception 
+    {
+    	session.sendMessage(new TextMessage("Welcome"));
+    }
+    
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message)
+			throws InterruptedException, IOException {
+
+		String payload = message.getPayload();
+		JSONObject jsonObject = new JSONObject(payload);
+		session.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + " how may we help you?"));
+	}
+	
+	
+	
+	
+
+}
